dfe0992a31043491343df532e1e53f4317c01919
SandrineB <s19borys@gmail.com>
2023-05-19 14:06:50 +0200

    intégration css carousel page d'accueil + intégration js ...blocage sur un element de js : classList

--- Accueil/accueil.js
+++ Accueil/accueil.js
@@ -1,13 +1,22 @@
-const slider = document.querySelector(".carousel-slider");
-const slides = document.querySelectorAll(".carousel-slide");
-let currentIndex = 0;
+let img_slider = document.getElementsByClassName('img_slider');
 
-function nextSlide() {
-  currentIndex++;
-  if (currentIndex >= slides.length) {
-    currentIndex = 0;
-  }
-  slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+let etape = 0;// affichage 1ère image
+
+let nbr_img = img_slider.length; // nombre d'images dans le slider
+
+let precedent = document.querySelector('.precedent');
+let suivant = document.querySelector('.suivant');
+
+function enleverActiveImages(){
+    for (let i=0; i< nbr_img; i++){
+        img_slider[i].classList.remove('active'); //rechercher à l'intérieur mes images et je supprime celle indiquée comme active
+    }
 }
 
-setInterval(nextSlide, 10000);
+suivant.addEventListener('click', function(){
+    etape++;
+    enleverActiveImages();
+    img_slider[etape].classList.add('active');
+})
+
+
