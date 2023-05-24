0d6814bc79f666e0f95d8150b6c070f74d453a06
SandrineB <s19borys@gmail.com>
2023-05-24 14:38:19 +0200

    finalisation du carousel de la page d'accueil : accueil .js

--- Front/accueil.js
+++ Front/accueil.js
@@ -1,5 +1,7 @@
+// récupération de l'ensemble des images
 let img_slider = document.getElementsByClassName('img_slider');
 
+// attribution de la valeur à la première image
 let etape = 0;// affichage 1ère image
 
 let nbr_img = img_slider.length; // nombre d'images dans le slider
@@ -7,16 +9,40 @@ let nbr_img = img_slider.length; // nombre d'images dans le slider
 let precedent = document.querySelector('.precedent');
 let suivant = document.querySelector('.suivant');
 
+//Lors de chaque clique, désactive l'image précédente
 function enleverActiveImages(){
-    for (let i=0; i< nbr_img; i++){
+    for (let i=0; i < nbr_img; i++){
         img_slider[i].classList.remove('active'); //rechercher à l'intérieur mes images et je supprime celle indiquée comme active
     }
 }
 
+//Permet d'activer le bouton droit du slider
 suivant.addEventListener('click', function(){
     etape++;
+    if(etape >=nbr_img){
+        etape = 0;
+    }
+    enleverActiveImages();
+    img_slider[etape].classList.add('active');
+})
+ 
+//Permet d'activer le bouton gauche du slider
+precedent.addEventListener('click', function(){
+    etape--;
+    if(etape < 0 ){
+        etape = nbr_img -1;
+    }
     enleverActiveImages();
     img_slider[etape].classList.add('active');
 })
 
+//Permet de faire défiler automatiquement après 3 secondes
+setInterval(function(){
+    etape++;
+    if(etape >=nbr_img){
+        etape = 0;
+    }
+    enleverActiveImages();
+    img_slider[etape].classList.add('active');
+},3000)
 
